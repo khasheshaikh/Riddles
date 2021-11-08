507065046362be51030f0c89f742c7b5036e6853
khasheshaikh <khasheshaikh@gmail.com>
2021-11-08 18:51:14 +0530

    done

---
+++ Riddles/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import {Container} from 'react-bootstrap'
+
+import {Home} from './components/Home'
+
+
+function App() {
+  return (
+    <>
+    <Home/>
+    </>
+  );
+}
+
+export default App;
---
+++ Riddles/src/components/Home.jsx
@@ -0,0 +1,32 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+import './Complete.css'
+import { Button, Card,Container } from "react-bootstrap";
+
+export const Home = () => {
+  const [joke, setJoke] = useState("");
+  const [fetching, setFetching] = useState(false)
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await axios(
+        "https://us-central1-dadsofunny.cloudfunctions.net/DadJokes/random/type/general"
+      );
+      //console.log(result.data)
+      setJoke(`${result.data[0].setup} ${result.data[0].punchline}`);
+    };
+    fetchData();
+  }, [fetching]);
+
+  return (
+    <>
+    <Card className="back">
+      <Card className="main">
+        
+        <h2>{joke}</h2>
+        <Button variant="primary" onClick={()=>setFetching(!fetching)}>next joke</Button>
+      </Card>
+    </Card>
+    </>
+  );
+};
